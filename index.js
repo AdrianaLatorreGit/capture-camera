71d5f64f5fd3855952b1fa894b73cebacd752bba
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-03-25 13:13:24 -0300

    adding information

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//getting myelements by Id
 const button = document.getElementById("button");
 const p = document.getElementById("text");
 const div = document.getElementById("container");
 
+//code that generates random color
 const redGreenBlue = () => {
   const red = Math.round(Math.random() * 256);
   const green = Math.round(Math.random() * 256);
@@ -11,4 +13,5 @@ const redGreenBlue = () => {
   p.innerText = `rgb(${red}, ${green}, ${blue})`;
 };
 
+//button that prints color data
 button.addEventListener("click", redGreenBlue);
